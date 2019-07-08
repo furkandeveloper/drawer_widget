d2c025d00c58b289745f1ed8b68b538d62e193e0
furkandeveloper <Furkan.dvlp@gmail.com>
2019-07-08 18:13:02 +0300

    project update

---
+++ drawer_widget/lib/screens/homePageScreen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePageScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // bottom overflowed hatası için çözüm;
+      resizeToAvoidBottomPadding: false,
+      appBar: AppBar(
+        title: Text(
+          "Drawer Widget",
+          textDirection: TextDirection.ltr,
+        ),
+      ),
+    );
+  }
+}
